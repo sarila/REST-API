7f2bef07a83360e49d648c85c87850f95f42d039
sarila <ngakhusisarila@gmail.com>
2021-01-13 12:34:01 +0545

    Creating Endpoint

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Restricts buyer to only two methods index and show
+Route::resource('buyers', 'BuyerController', ['only' => ['index', 'show']]);
 
+Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+
+Route::resource('products', 'ProductController', ['only' => ['index', 'show']]);
+
+Route::resource('sellers', 'SellerController', ['only' => ['index', 'show']]);
+
+Route::resource('transactions', 'TransactionController', ['only' => ['index', 'show']]);
+
+Route::resource('users', 'userController', ['except' => ['create', 'edit']]);

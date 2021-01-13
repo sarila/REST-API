47bcfd6a1a27036d6dad1da1d3d4502edad8d74f
sarila <ngakhusisarila@gmail.com>
2021-01-13 13:15:19 +0545

    Implementation of relationship between models

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,41 @@
 
 namespace App;
 
+use App\Seller;
+use App\Transaction;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	const AVAILABLE_PRODUCT = 'available';
+	const UNAVAILABLE_PRODUCT = 'unavailable';
+
+	protected $fillable =[
+		'name',
+		'description',
+		'quantity',
+		'status',
+		'image',
+		'selelr_id',
+	];
+
+	public function isAvailable(){
+		return $this->status == Product::AVAILABLE_PRODUCT;
+	}
+	public function seller()
+	{
+		return $this->belongsTo(Seller::class);
+	}
+
+	public function transactions()
+	{
+		return $this->belongsTo(Transaction::class);
+	}
+
+	public function categories(){
+		 return $this->belongsToMany(Category::class);
+	}
+
+
 }

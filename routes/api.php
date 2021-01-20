f237dc19b5e4e93a34d5171fe4f3852768173035
sarila <ngakhusisarila@gmail.com>
2021-01-20 16:57:19 +0545

    Implementation of Opeations for product with other models

--- routes/api.php
+++ routes/api.php
@@ -19,22 +19,35 @@ Route::resource('buyers.products', 'BuyerProductController', ['only' => ['index'
 Route::resource('buyers.sellers', 'BuyerSellerController', ['only' => ['index']]);
 Route::resource('buyers.categories', 'BuyerCategoryController', ['only' => ['index']]);
 
+
+//Routes related Categories
 Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
 Route::resource('categories.products', 'CategoryProductController', ['only' => ['index']]);
 Route::resource('categories.sellers', 'CategorySellerController', ['only' => ['index']]);
 Route::resource('categories.transactions', 'CategoryTransactionController', ['only' => ['index']]);
 Route::resource('categories.buyers', 'CategoryBuyerController', ['only' => ['index']]);
 
-Route::resource('products', 'ProductController', ['only' => ['index', 'show']]);
 
+//Routes related Product
+Route::resource('products', 'ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions', 'ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers', 'ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'ProductCategoryController', ['only' => ['index', 'destroy', 'update']]);
+
+
+//Routes related Seller
 Route::resource('sellers', 'SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', 'SellerTransactionController', ['only' => ['index']]);
 Route::resource('sellers.categories', 'SellerCategoryController', ['only' => ['index']]);
 Route::resource('sellers.buyers', 'SellerBuyerController', ['only' => ['index']]);
 Route::resource('sellers.products', 'SellerProductController', ['except' => ['create', 'edit', 'show']]);
 
+
+//Routes related Transactions
 Route::resource('transactions', 'TransactionController', ['only' => ['index', 'show']]);
 Route::resource('transactions.categories', 'TransactionCategoryController', ['only' => ['index']]);
 Route::resource('transactions.sellers', 'TransactionSellerController', ['only' => ['index']]);
 
+
+//Routes for user
 Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);

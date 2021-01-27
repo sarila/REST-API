3297593d1f2b1059cfb0ad4e8f9d1cb2b3c9104a
sarila <ngakhusisarila@gmail.com>
2021-01-27 14:52:17 +0545

    authentication made and register route deleted

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,17 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+//Password Reset Route
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::get('/home', 'HomeController@index')->name('home');

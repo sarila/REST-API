3d561ee954ecdfd7980d50c8b66fbb56a1a79f0a
sarila <ngakhusisarila@gmail.com>
2021-02-07 16:25:15 +0545

    Retry helper added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,14 +30,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
+        //retry(times, function(), quantity_of_time)
         User::created(function($user) {
-            Mail::to($user)->send(new UserCreated($user));
+            retry(5, function() use ($user) {
+              Mail::to($user)->send(new UserCreated($user));  
+            }, 100);
         });
 
         User::updated(function($user) {
             if($user->isDirty('email')){
-                Mail::to($user)->send(new UserMailChanged($user));
+                retry(5, function() use ($user) {
+                  Mail::to($user)->send(new UserMailChanged($user));  
+                } , 100);
             };
         });
 
